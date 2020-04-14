2937f2a5704ae83c8675a1f7486397b55d7b9f20
gul hossain <spicymangojungle@gmail.com>
2020-04-13 21:46:05 -0700

    add router to the server

--- api/server.js
+++ api/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const accountsRouter = require('../accounts/accountRouter');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.get("/", (req,res) => {
+    res.send("<h1>Server Working</h1>");
+})
 
 module.exports = server;
